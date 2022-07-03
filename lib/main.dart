b756f3e91f6e8466db6086210410ff92d17f3c19
Messi <rostandmessi2@gmail.com>
2022-07-03 12:48:09 +0100

    flutterbootcamp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: camel_case_types, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'dart:math';
+
+
 
 void main() => runApp(const MyApp());
 
@@ -17,43 +18,25 @@ class MyApp extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 20, 63, 99),
         appBar: AppBar( title:const Text('MIRsquared MagicBall',style: TextStyle(fontFamily:'Pacifico'),),
         backgroundColor: Color.fromARGB(255, 31, 88, 185),),
-        body: magicBall(),
+        body: Xyloo(),
         ),
     );
   }
 }
 
-class magicBall extends StatefulWidget {
-  const magicBall({Key? key}) : super(key: key);
+class Xyloo extends StatelessWidget {
+  const Xyloo({Key? key}) : super(key: key);
 
-  @override
-  State<magicBall> createState() => _magicBallState();
-}
-
-class _magicBallState extends State<magicBall> {
-  int number = 4;
-  void changeNum (){
-    setState(() {
-      number = Random().nextInt(5) + 1; 
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Expanded(
-              child: TextButton(
-                onPressed:() => changeNum(), 
-              child: Image.asset('Magicballpics/ball$number.png'),
-              ),
-            ),
-          ),
-        ],
+    return SafeArea(child: Center(
+      child: TextButton(child: const Text('Click me'),
+      onPressed:() {
+       
+     
+      },
+      style: TextButton.styleFrom(),
       ),
-    );
+    ),);
   }
 }
